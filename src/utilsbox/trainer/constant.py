ad5f8d3827792dafeed31555516c9d36531674c9
yusijin02 <1207937423@qq.com>
2024-06-04 19:56:10 +0800

    [feat] trainer的基建

---
+++ src/utilsbox/trainer/constant.py
@@ -0,0 +1,5 @@
+class ConfigSuffix:
+    json = ["json", "JSON", ".json", ".JSON"]
+    yaml = ["yaml", "YAML", "yml", "YML", ".yaml", ".YAML", ".yml", ".YML"]
+    ini = ["ini", "INI", ".ini", ".INI"]
+    py = ["py", "PY", ".py", ".PY"]
