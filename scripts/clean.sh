fe700af553dc5eeff989e538b21714d09e316e1b
Chrisqcwx <3075401625@qq.com>
2025-10-05 16:14:09 +0800

    fix: script

---
+++ scripts/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf build
+rm -rf dist
+rm -rf src/phoenixcat.egg-info
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
+set -e
 git push
 
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
-
-rm -rf build
-rm -rf dist
 
